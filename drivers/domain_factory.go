ad60ba972ef718acf4e3d690e68828a727e5b846
rlzmtnck <54921947+rlzmtnck@users.noreply.github.com>
2021-12-30 18:30:28 +0700

    Check error

--- drivers/domain_factory.go
+++ drivers/domain_factory.go
@@ -2,18 +2,16 @@ package drivers
 
 import (
 	adminDomain "Hospital-Management-System/business/admins"
-	doctrDomain "Hospital-Management-System/business/doctors"
+	doctorDomain "Hospital-Management-System/business/doctors"
 	patientDomain "Hospital-Management-System/business/patients"
-  
+
 	adminDB "Hospital-Management-System/drivers/databases/admins"
 	doctorDB "Hospital-Management-System/drivers/databases/doctors"
 	patientDB "Hospital-Management-System/drivers/databases/patients"
-  
-	nurseDomain "Hospital-Management-System/business/nurses"
-	adminDB "Hospital-Management-System/drivers/databases/admins"
-	doctorDB "Hospital-Management-System/drivers/databases/doctors"
-	nurseDB "Hospital-Management-System/drivers/databases/nurses"
 
+	nurseDomain "Hospital-Management-System/business/nurses"
+
+	nurseDB "Hospital-Management-System/drivers/databases/nurses"
 
 	"gorm.io/gorm"
 )
@@ -30,8 +28,6 @@ func NewPatientRepository(conn *gorm.DB) patientDomain.Repository {
 	return patientDB.NewMysqlPatientRepository(conn)
 }
 
-
 func NewNurseRepository(conn *gorm.DB) nurseDomain.Repository {
 	return nurseDB.NewMysqlNurseRepository(conn)
 }
-
